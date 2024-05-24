2956c4ec6be7750603cf2334c4e1c97c51b0624b
João Araújo <jpedro.araujo21@gmail.com>
2024-05-24 20:41:07 -0300

    Exception handling - findById

--- src/main/java/com/tech/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tech/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.tech.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,12 @@ public class UserResource {
         List<User> users = userService.findAll();
         List<UserDTO> usersDto = users.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(usersDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        UserDTO userDTO = new UserDTO(user);
+        return ResponseEntity.ok().body(userDTO);
     }
 }
--- src/main/java/com/tech/workshopmongo/services/UserService.java
+++ src/main/java/com/tech/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.tech.workshopmongo.services;
 
 import com.tech.workshopmongo.domain.entities.User;
 import com.tech.workshopmongo.repositories.UserRepository;
+import com.tech.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
